b71fb45d36cb9b16ef8bd87136b6ac6b1744a1a9
sampahallen <sampahallen@gmail.com>
2025-10-21 16:23:25 +0000

    Enhance Cards and Texts components with improved styling and layout adjustments

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -25,28 +25,28 @@ const Cards = () => {
             key={index}
             onClick={() => handleActiveChange(index)}
           className={`flex flex-col rounded-3xl p-11 w-full max-w-md bg-white ${
-            plan.isActive === true ? "border border-purple-500" : "border-none"
+            plan.isActive === true ? "border-2 shadow-lg shadow-purple-500 border-purple-500" : "border-2"
           } cursor-pointer relative`}
         >
-          <div className={`absolute top-6 right-8 ${plan.isActive ? '' : 'hidden'}`}>
-            <img src={star} alt="" className="w-[30px] h-[30px] object-cover" />
+          <div className={`absolute top-5 right-7 ${plan.isActive ? '' : 'hidden'}`}>
+            <img src={star} alt="" className="w-[40px] h-[40px] object-contain" />
           </div>
           <div className="ml-9 my-5">
             <div className="flex flex-row gap-6 items-center">
-              <p className="text-[18px] text-gray-600">{plan.name}</p>
+              <p className="text-[20px] text-gray-600 font-bold leading-[24px]">{plan.name}</p>
               <p
                 className={`${
                   plan.mostPopular ? "" : "hidden"
-                } border border-green-400 rounded-xl p-1 text-center
-              text-[12px] font-semibold text-green-400 min-w-[120px]`}
+                } border border-green-500 rounded-xl p-1 text-center
+              text-[12px] font-semibold text-green-500 min-w-[150px] bg-green-50`}
               >
                 Most Popular
               </p>
             </div>
 
-            <h3 className="text-[30px] font-semibold ">{plan.price}</h3>
+            <h3 className="text-[35px] font-bold ">{plan.price}</h3>
           </div>
-          <div className="flex flex-col items-center justify-center mt-3">
+          <div className="flex flex-col items-center justify-center">
             <motion.button
             whileHover={{scale: 1.03}}
             whileTap={{scale: 0.95}}
@@ -59,7 +59,7 @@ const Cards = () => {
               Buy Plan
             </motion.button>
 
-            <div className="flex flex-row items-center justify-center gap-3">
+            <div className="flex flex-row items-center justify-center gap-4">
               {cards.map((card) => (
                 <img
                   src={card.image}
@@ -69,12 +69,12 @@ const Cards = () => {
               ))}
             </div>
           </div>
-          {plan.offers.map((offer) => (
+          {plan.offers.map((offer, offerIndex) => (
             <div>
               <p className="font-semibold text-[16px] mt-10 mb-2 ">
                 {offer.title}
               </p>
-              <ul>
+              <ul className="mb-4">
                 {offer.items.map((item) => (
                   <div className="flex flex-row  items-center justify-start gap-2">
                     <img
@@ -84,7 +84,7 @@ const Cards = () => {
                     />
                     <li className="my-2 text-[14px]">{item.title}</li>
                     <p
-                      className={`text-[12px] text-purple-500 ml-3 ${
+                      className={`text-[12px] bg-purple-200 rounded-xl min-w-[80px] flex items-center justify-center text-purple-500 ml-3 ${
                         item.boost ? "block" : "hidden"
                       }`}
                     >
@@ -94,8 +94,14 @@ const Cards = () => {
                       </span>
                     </p>
                   </div>
-                ))}
+                ))} 
               </ul>
+              {
+                offerIndex < plan.offers.length -1 && (
+                    <hr className="border-t border-gray-400 my-4"/>
+                )
+              }
+             
             </div>
           ))}
         </motion.div>
--- src/components/Texts.jsx
+++ src/components/Texts.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
+import { useState } from "react";
 
 const Texts = () => {
+  const [toggle, setToggle] = useState("left");
   return (
-    <section className='flex flex-col items-center justify-center text-center gap-6 mt-10'>
-        <h3 className='text-purple-500 text-[18px] font-bold leading-[100%]'>PRICING</h3>
-        <h1 className='text-[40px] font-bold leading-[100%]'>Choose the plan that fits your needs</h1>
-        <div className='flex flex-row justify-between items-center min-w-[250px] mt-7 text-center'>
-            <p className='text-[16px]'>Monthly</p>
-            <div className='flex flex-col text-[16px]'>
-                 <p>Annually</p>
-                 <p className='text-purple-500'>Save 30%</p>
-            </div>
-        </div>
-    </section>
-  )
-}
+    <section className="flex flex-col items-center justify-center text-center gap-6 mt-10">
+      <h3 className="text-purple-500 text-[18px] font-semibold leading-[100%]">
+        PRICING
+      </h3>
+      <h1 className="text-[40px] font-bold leading-[100%]">
+        Choose the plan that fits your needs
+      </h1>
 
-export default Texts
+      <div className="flex flex-row justify-between items-center min-w-[350px] mt-7 text-center border rounded-xl p-2 h-[90px]">
+        <p
+          onClick={() => setToggle("left")}
+          className={`cursor-pointer flex items-center justify-center text-[16px] w-1/2 h-[75px] p-6 z-10 ${toggle === "left" ? 'bg-gray-200 rounded-lg' : ''}`}
+        >
+          Monthly
+        </p>
+        <div
+          onClick={() => setToggle("right")}
+          className={`flex flex-col text-[16px] h-[75px] items-center justify-center cursor-pointer w-1/2 p-6 z-10 ${toggle === "right" ? 'bg-gray-100 rounded-lg' : ''}`}
+        >
+          <p>Annually</p>
+          <p className="text-purple-500">Save 30%</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Texts;
