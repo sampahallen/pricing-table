c629b001260d65747f0695598b6794fa44fbea7e
sampahallen <sampahallen@gmail.com>
2025-10-23 12:07:13 +0000

    Update Cards and Texts components for improved layout, spacing, and responsiveness

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -17,34 +17,34 @@ const Cards = () => {
         setplans(updated)
     }
   return (
-    <section className="flex flex-row flex-wrap gap-5 items-stretch justify-center my-10 sm: p-5">
+    <section className="flex flex-row flex-wrap gap-5 items-stretch justify-center sm:p-0 p-5 mt-5 mb-10">
       {plans.map((plan, index) => (
         <motion.div
             whileHover={{scale: 1.01}}
             whileTap={{scale: 0.95}}
             key={index}
             onClick={() => handleActiveChange(index)}
-          className={`flex flex-col rounded-3xl p-11 w-full  sm:max-w-[350px] sm:h-hit bg-white ${
+          className={`flex flex-col rounded-3xl p-7 w-full  sm:max-w-[400px] sm:h-hit bg-white ${
             plan.isActive === true ? "border-2 shadow-lg shadow-purple-500 border-purple-500" : "border-2"
           } cursor-pointer relative`}
         >
           <div className={`absolute top-4 right-5 ${plan.isActive ? '' : 'hidden'}`}>
             <img src={star} alt="" className="w-[30px] h-[30px] object-contain" />
           </div>
-          <div className="ml-9 my-5">
-            <div className="flex flex-row gap-6 items-center">
-              <p className="text-[20px] text-gray-600 font-bold leading-[24px]">{plan.name}</p>
+          <div className="ml-1 mb-5 mt-1">
+            <div className="flex flex-row gap-6 items-start">
+              <p className="text-[20px] text-gray-600 font-bold">{plan.name}</p>
               <p
                 className={`${
                   plan.mostPopular ? "" : "hidden"
                 } border border-green-500 rounded-xl p-1 text-center
-              text-[12px] font-semibold text-green-500 min-w-[150px] bg-green-50`}
+              text-[10px] font-semibold text-green-500 min-w-[150px] bg-green-50`}
               >
                 Most Popular
               </p>
             </div>
 
-            <h3 className="text-[35px] font-bold ">{plan.price}</h3>
+            <h3 className="text-[35px] tracking-wider font-bold ">{plan.price}</h3>
           </div>
           <div className="flex flex-col items-center justify-center">
             <motion.button
@@ -59,19 +59,19 @@ const Cards = () => {
               Buy Plan
             </motion.button>
 
-            <div className="flex flex-row items-center justify-center gap-4">
+            <div className="flex flex-row items-center justify-center gap-4 mt-1 mb-5">
               {cards.map((card) => (
                 <img
                   src={card.image}
                   alt=""
-                  className="sm:w-[50px] sm:h-[50px] w-[35px] h-[35px] object-contain"
+                  className="sm:w-[50px] sm:h-[35px] w-[35px] h-[35px] object-contain"
                 />
               ))}
             </div>
           </div>
           {plan.offers.map((offer, offerIndex) => (
-            <div>
-              <p className="font-semibold text-[16px] mt-10 mb-2 ">
+            <div >
+              <p className="font-semibold text-[16px] mb-1 ">
                 {offer.title}
               </p>
               <ul className="mb-4">
@@ -82,9 +82,9 @@ const Cards = () => {
                       alt=""
                       className="w-[20px] h-[20px] object-contain"
                     />
-                    <li className="my-2 text-[14px]">{item.title}</li>
+                    <li className="my-1 text-[12px]">{item.title}</li>
                     <p
-                      className={`text-[12px] bg-purple-200 rounded-xl min-w-[80px] flex items-center justify-center text-purple-500 ml-3 ${
+                      className={`text-[10px] bg-purple-200 rounded-xl min-w-[80px] flex items-center justify-center text-purple-500 ml-3 ${
                         item.boost ? "block" : "hidden"
                       }`}
                     >
@@ -98,7 +98,7 @@ const Cards = () => {
               </ul>
               {
                 offerIndex < plan.offers.length -1 && (
-                    <hr className="border-t border-gray-400 "/>
+                    <hr className="border-t border-gray-400 mb-3"/>
                 )
               }
              
--- src/components/Texts.jsx
+++ src/components/Texts.jsx
@@ -3,24 +3,24 @@ import { useState } from "react";
 const Texts = () => {
   const [toggle, setToggle] = useState("left");
   return (
-    <section className="flex flex-col items-center justify-center text-center gap-6 mt-10">
+    <section className="flex flex-col items-center justify-center text-center gap-3 mt-5">
       <h3 className="text-purple-500 text-[18px] font-semibold leading-[100%]">
         PRICING
       </h3>
-      <h1 className="text-[40px] font-bold leading-[100%]">
+      <h1 className="text-[35px] font-bold leading-[100%]">
         Choose the plan that fits your needs
       </h1>
 
-      <div className="flex flex-row justify-between items-center min-w-[350px] mt-7 text-center border rounded-xl p-1 h-[72px]">
+      <div className="flex flex-row justify-between items-center min-w-[200px] mt-4 text-center border rounded-xl p-1 h-[55px]">
         <p
           onClick={() => setToggle("left")}
-          className={`cursor-pointer flex items-center justify-center text-[16px] w-1/2 h-[64px] p-6 ${toggle === "left" ? 'bg-gray-200 rounded-lg' : ''}`}
+          className={`cursor-pointer flex items-center justify-center text-[9px] font-semibold w-1/2 h-[43px] p-6 ${toggle === "left" ? 'bg-gray-200 rounded-lg' : ''}`}
         >
           Monthly
         </p>
         <div
           onClick={() => setToggle("right")}
-          className={`flex flex-col text-[16px] h-[64px] items-center justify-center cursor-pointer w-1/2 p-6 ${toggle === "right" ? 'bg-gray-100 rounded-lg' : ''}`}
+          className={`flex flex-col text-[9px] h-[43px] font-semibold items-center justify-center cursor-pointer w-1/2 p-6 ${toggle === "right" ? 'bg-gray-100 rounded-lg' : ''}`}
         >
           <p>Annually</p>
           <p className="text-purple-500">Save 30%</p>
